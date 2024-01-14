e381f3f3a997de5c70fff5000e888fe7ee66cba6
christian89-alt <c.duboc43@gmail.com>
2024-01-14 20:17:27 +0100

    header&footer

---
+++ public/partial/footer.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<footer class="site-footer container text-center">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="main-footer">
+					<ul class="social">
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">Instagram</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 copyright">
+				<p>Copyright &copy; 2084 <a href="#">Company Name</a></p>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
