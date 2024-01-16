edbeb0fb55d005c7e321f579d5393b64561eef4e
christian89-alt <c.duboc43@gmail.com>
2024-01-16 13:44:35 +0100

    burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,4 @@
-const burger = document.getElementById('burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-})
+const toggleMenu = () => {
+    document.body.classList.toggle("open");
+  };
+  
