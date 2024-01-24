8c5c0a53bbe38b41bdf6db891f1d7415b71ffae8
christian89-alt <c.duboc43@gmail.com>
2024-01-24 16:19:18 +0100

    update phpmailer

--- Controller/mail.php
+++ Controller/mail.php
@@ -2,18 +2,55 @@
 
 
 namespace Controller;
+
 use PHPMailer\PHPMailer\PHPMailer;
-class Mail {
+use PHPMailer\PHPMailer\SMTP;
 
-    public function __construct($email,$sujet,$text){
+class Mail
+{
 
-      $mail = new PHPMailer();
+    public function __construct($email, $sujet, $text);
+    {
+        private $mailClass;
+        $mail = new PHPMailer();
+
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'user@example.com';                     //SMTP username
+        $mail->Password   = 'fbjh gndd nfgc kidq';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+        $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+
+       
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $this->mailClass = $mail;
+        return this;
+
+        $mail->send();
+        echo 'Message has been sent';
+    }
+    public function send()
+    {
+        
+       $resp= $this->mailClass->send();
+       return $resp;
 
 
     }
-    public function send (){
-
-    }
-
 }
-?>
--- class/index.php
+++ class/index.php
@@ -3,13 +3,13 @@
 Use Controller\Mail;
 require 'vendor/autoload.php';
 
-if(isset($ POST ['text']) && !empty($ POST ['text']) &&
-isset($ POST ['email']) && !empty($ POST ['email']) &&
-isset($ POST ['message']) && !empty($ POST ['message']) 
+if(isset($POST ['text']) && !empty($POST ['text']) &&
+isset($POST ['email']) && !empty($POST ['email']) &&
+isset($POST ['message']) && !empty($POST ['message']) 
 ){
     
-    $mail = new Mail($ POST ['text'],$ POST ['email'],$ POST ['message']);
-    $mail->send();
+    $mail = new Mail($POST ['text'],$POST ['email'],$POST ['message']);
+    $resp= $mail->send ();
 
 
     
