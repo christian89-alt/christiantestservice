60c77491e175058da114e43f3f0ba6d992074ba7
christian89-alt <c.duboc43@gmail.com>
2024-01-24 16:29:12 +0100

    update phpmailer

--- class/index.php
+++ class/index.php
@@ -8,8 +8,11 @@ isset($POST ['email']) && !empty($POST ['email']) &&
 isset($POST ['message']) && !empty($POST ['message']) 
 ){
     
-    $mail = new Mail($POST ['text'],$POST ['email'],$POST ['message']);
+    $mail= new Mail($POST ['text'],$POST ['email'],$POST ['message']);
     $resp= $mail->send ();
+    $info= $resp 'succès': 'danger'?;
+    $info= 'Votre message à bien été envoyé':'Message non envoyé. Vérifier que tout les champs sont biens remplis.';
+
 
 
     
