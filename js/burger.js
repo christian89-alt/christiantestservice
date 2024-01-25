9a684adcc0d3ef2eb1f02cf82895eb5c5670d5c4
christian89-alt <c.duboc43@gmail.com>
2024-01-25 15:36:12 +0100

    update

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,16 @@
-const toggleMenu = () => {
-    document.body.classList.toggle("open");
-  };
-  
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
