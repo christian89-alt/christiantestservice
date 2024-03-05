fc9a3f48d773ccee6b6dab451a87ff60921e20b6
christian89-alt <c.duboc43@gmail.com>
2024-03-05 12:20:41 +0100

    header&footer php

---
+++ partial/footer.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<footer id="copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">
+            Copyright &copy; Offshores Services
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ partial/header.php
@@ -0,0 +1,92 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Conseil entreprise | Développement web & mobile</title>
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="stylesheet" href="assets/css/animate.min.css" />
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="assets/css/burger.css"
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="assets/css/christianservice-style.css" />
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.singlePageNav.min.js"></script>
+  <script src="js/typed.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body id="top">
+  <!-- start preloader -->
+  <div class="preloader">
+    <div class="sk-spinner sk-spinner-wave">
+      <div class="sk-rect1"></div>
+      <div class="sk-rect2"></div>
+      <div class="sk-rect3"></div>
+      <div class="sk-rect4"></div>
+      <div class="sk-rect5"></div>
+    </div>
+  </div>
+  <!-- end preloader -->
+
+  <!-- start header -->
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-4 col-xs-12">
+          <p>
+            <i class="fa fa-phone"></i><span> Téléphone</span>+33 7 67 53 36
+            25
+          </p>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-12">
+          <p>
+            <i class="fa fa-envelope-o"></i><span> Email</span><a href="#">c.duboc43@gmail.com</a>
+          </p>
+        </div>
+        <div class="col-md-5 col-sm-4 col-xs-12">
+          <ul class="social-icon">
+            <li><a href="./contact-us.php"><span>ME CONTACTER</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- end header -->
+
+   <!-- start navigation -->
+  
+   <div class="container-fluid main">
+
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> 
+          </button>
+          <a class="navbar-brand" href="./index.html">Offshore Services </a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="./index.html">Accueil</a></li>
+            <li><a href="./about.html">Le cabinet</a></li>
+            <li><a href="./web-developpement.html">Web & Mobil</a></li>
+            <li><a href="./agence_commerciale.html">Assistance commerciale</a></li>
+            <li><a href="./offshore.html">Offshore</a></li>
+            <li><a href="./contact-us.php">Contact</a></li> 
+          </ul>
+        </div>
+      </div>
+    </nav>
