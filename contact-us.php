1c48186ca87d0b60297c19e3cf96d3b65ceaacb1
christian89-alt <c.duboc43@gmail.com>
2024-03-14 11:47:27 +0100

    update

--- contact-us.php
+++ contact-us.php
@@ -1,181 +1,264 @@
 <?php
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
+  <!-- Basic -->
   <meta charset="utf-8" />
-
-  <title>Conseil entreprise | Développement web & mobile</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
   <meta name="keywords" content="" />
   <meta name="description" content="" />
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="./assets/css/animate.min.css" />
-  <link rel="stylesheet" href="./assets/css/burger.css" />
-  <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="./assets/css/font-awesome.min.css" />
-  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
-  <link rel="stylesheet" href="./assets/css/christianservice-style.css" />
-  <script src="js/jquery.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.singlePageNav.min.js"></script>
-  <script src="js/typed.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script src="js/custom.js"></script>
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+  <title> Brincht </title>
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,600,700&display=swap" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="assets/css/responsive.css" rel="stylesheet" />
 </head>
 
-<body id="top">
-  <!-- start preloader -->
-  <div class="preloader">
-    <div class="sk-spinner sk-spinner-wave">
-      <div class="sk-rect1"></div>
-      <div class="sk-rect2"></div>
-      <div class="sk-rect3"></div>
-      <div class="sk-rect4"></div>
-      <div class="sk-rect5"></div>
-    </div>
-  </div>
-  <!-- end preloader -->
+<body class="sub_page">
 
-  <!-- start header -->
-  <div class="page">
-    <nav id="colorlib-main-nav" role="navigation">
-      <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
-      <div class="js-fullheight colorlib-table">
-        <div class="img" style="background-image: url(assets/img/pop3-bg.jpg);"></div>
-        <div class="colorlib-table-cell js-fullheight">
-          <div class="row no-gutters">
-            <div class="col-md-12 text-center">
-              <h1 class="mb-4"><a href="index.html" class="logo">Company Logo</a></h1>
-              <ul>
-                <li class="active"><a href="index.html"><span>Home</span></a></li>
-                <li><a href="about.html"><span>Le cabinet</span></a></li>
-                <li><a href="offshore.html"><span>Offshore</span></a></li>
-                <li><a href="web-developpement.html"><span>Web & Mobile</span></a></li>
-                <li><a href="agence_commerciale.html"><span>Agent commercial</span></a></li>
-                <li><a href="contact.html"><span>Contact</span></a></li>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <div class="hero_bg_box">
+      <img src="assets/img/hero-bg.jpg" alt="">
+    </div>
+
+    <header class="header_section">
+      <div class="header_top">
+        <div class="container-fluid">
+          <div class="contact_link-container">
+            <a href="" class="contact_link1">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>
+                Lorem ipsum dolor sit amet
+              </span>
+            </a>
+            <a href="" class="contact_link2">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                Call : +01 1234567890
+              </span>
+            </a>
+            <a href="" class="contact_link3">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span>
+                demo@gmail.com
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="header_bottom">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="index.html">
+              <span>
+                Brincht
+              </span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class=""></span>
+            </button>
+
+            <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+              <ul class="navbar-nav  ">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="about.html"> About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="service.html"> Services </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="blog.html"> Blog </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="testimonial.html">Testimonial</a>
+                </li>
               </ul>
             </div>
-          </div>
+          </nav>
         </div>
       </div>
-    </nav>
-
-    <div id="colorlib-page">
-      <header>
-        <div class="container">
-          <div class="colorlib-navbar-brand">
-            <a class="colorlib-logo" href="index.html">Offshore Services</a>
-          </div>
-          <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-        </div>
-      </header>
-
-      <section class="hero-wrap js-fullheight">
-        <div class="container px-0">
-          <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center"
-            data-scrollax-parent="true">
-            <div class="col-md-12 ftco-animate text-center">
-              <div class="desc">
-                <h1>Website Menu #10</h1>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-    </div>
+    </header>
+    <!-- end header section -->
   </div>
- 
-  <!-- start home -->
-  <section id="home">
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding">
     <div class="container">
       <div class="row">
-        <div class="col-md-offset-2 col-md-8">
-          <h1 class="wow fadeIn" data-wow-offset="50" data-wow-delay="0.9s">
-            Une expertise pour <span>l'entreprise et le créateur </span>
-          </h1>
-          <div class="element">
-            <div class="sub-element">
-              Offshore service, spécialiste de l'entreprise
-            </div>
-            <div class="sub-element">
-              Développement web et mobile, gestion de projet web
-            </div>
-            <div class="sub-element">
-              Développement et optimisation entreprise
+        <div class="col-md-6 ">
+          <div class="img_container">
+            <div class="img-box">
+              <img src="images/a1.jpg" class="about_img1" alt="" />
+              <img src="images/a2.jpg" class="about_img2" alt="" />
             </div>
           </div>
-          <a data-scroll href="#about" class="btn btn-default wow fadeInUp" data-wow-offset="50" data-wow-delay="0.6s">GET STARTED</a>
+        </div>
+        <div class="col-md-6 ">
+          <div class="detail-box">
+            <div class="heading_container ">
+              <h2>
+                Who Are We?
+              </h2>
+            </div>
+            <p>
+              Reprehenderit quia placeat odio. Consequuntur sunt quo expedita a. Ipsam nostrum maxime facere porro nemo itaque beatae, ad voluptate iste placeat debitis dolore asperiores fugiat totam eveniet consequatur natus. Aliquam.
+            </p>
+            <p>
+              Sed alias, labore iusto facilis doloribus fugiat totam officia corporis vitae error, voluptates nihil veniam et dicta eveniet deserunt at delectus placeat eligendi ipsum voluptatibus aliquam tempore molestiae veritatis. Aspernatur.
+            </p>
+            <div class="btn-box">
+              <a href="">
+                Read More
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
-  <!-- end home -->
 
-  <!-- start contact -->
-  <section id="contact">
+  <!-- end about section -->
+
+  <!-- info section -->
+  <section class="info_section ">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
-          <h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">
-            NOUS <span>CONTACTER</span>
-          </h2>
+        <div class="col-md-3">
+          <div class="info_logo">
+            <a class="navbar-brand" href="index.html">
+              <span>
+                Brincht
+              </span>
+            </a>
+            <p>
+              Libero minus ab reprehenderit atque cum praesentium placeat quos est enim facere cumque. Numquam pariatur alias
+            </p>
+          </div>
         </div>
-        <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-offset="50" data-wow-delay="0.9s">
-          <form action="#" method="post">
-            <label>NOM</label>
-            <input name="fullname" type="text" class="form-control" id="fullname" />
-
-            <label>EMAIL</label>
-            <input name="email" type="email" class="form-control" id="email" />
-
-            <label>MESSAGE</label>
-            <textarea name="message" rows="4" class="form-control" id="message"></textarea>
-
-            <input type="submit" class="form-control" />
-          </form>
+        <div class="col-md-3">
+          <div class="info_links">
+            <h5>
+              Useful Link
+            </h5>
+            <ul>
+              <li>
+                <a href="">
+                  Dolor sit amet, consectetur
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Magna aliqua. Ut enim ad
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Minim veniam placeat
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Quisdotempor incididunt
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight" data-wow-offset="50" data-wow-delay="0.6s">
-          <address>
-            <p class="address-title">Adresse</p>
-            <span>N'hésitez pas à me contacter pour que nous échangions sur votre projet!</span>
-            <p><i class="fa fa-phone"></i> + 33 7 67 53 36 25</p>
-            <p><i class="fa fa-envelope-o"></i> c.duboc43@gmail.com</p>
-
-          </address>
-          <ul class="social-icon">
-            <li>
-              <h4>WE ARE SOCIAL</h4>
-            </li>
-            <li><a href="#" class="fa fa-facebook"></a></li>
-            <li><a href="#" class="fa fa-twitter"></a></li>
-            <li><a href="#" class="fa fa-instagram"></a></li>
-          </ul>
+        <div class="col-md-3">
+          <div class="info_info">
+            <h5>
+              Contact Us
+            </h5>
+          </div>
+          <div class="info_contact">
+            <a href="" class="">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>
+                Lorem ipsum dolor sit amet,
+              </span>
+            </a>
+            <a href="" class="">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                Call : +01 1234567890
+              </span>
+            </a>
+            <a href="" class="">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span>
+                demo@gmail.com
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info_form ">
+            <h5>
+              Newsletter
+            </h5>
+            <form action="#">
+              <input type="email" placeholder="Enter your email">
+              <button>
+                Subscribe
+              </button>
+            </form>
+            <div class="social_box">
+              <a href="">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-youtube" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
-  <!-- end contact -->
 
-  <!-- start copyright -->
-  <footer id="copyright">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 text-center">
-          <p class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">
-            Copyright &copy; Offshores Services
-          </p>
-        </div>
-      </div>
-    </div>
+  <!-- end info_section -->
+
+
+
+
+  <!-- footer section -->
+  <footer class="container-fluid footer_section">
+    <p>
+      &copy; <span id="currentYear"></span> All Rights Reserved. Design by
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
   </footer>
-  <!-- end copyright -->
-  <script src="js/popper.js"></script>
+  <!-- footer section -->
 
-  <script src="js/main.js"></script>
+  <script src="assets/ js/jquery-3.4.1.min.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/custom.js"></script>
 </body>
 
 </html>
