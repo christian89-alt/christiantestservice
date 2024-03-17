d0374049590fa9e34e28abbb4d750c7daa1bf39f
christian89-alt <c.duboc43@gmail.com>
2024-03-17 12:21:46 +0100

    update

--- partial/footer.php
+++ partial/footer.php
@@ -6,8 +6,8 @@
   <!-- footer section -->
   <footer class="container-fluid footer_section">
     <p>
-      &copy; <span id="currentYear"></span> All Rights Reserved. Design by
-      <a href="https://html.design/">Free Html Templates</a>
+      &copy; <span id="currentYear"></span> Tous droit réservés Offshore Service
+      <a href="https://Offshoresservice.com/">Offshore Service</a>
     </p>
   </footer>
   <!-- footer section -->
@@ -15,4 +15,16 @@
   <script src="assets/js/jquery-3.4.1.min.js"></script>
   <script src="assets/js/bootstrap.js"></script>
   <script src="assets/js/custom.js"></script>
+  <script>
+window.axeptioSettings = {
+  clientId: "6165d1d27df77648d1f6329c",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
+</script>
 </body>
+</html>
