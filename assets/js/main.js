43f35fabcb74de66d5c1a34e1fbf04850bb90850
christian89-alt <c.duboc43@gmail.com>
2024-03-17 15:29:11 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,20 @@
 	burgerMenu();
 
 
+
+})(jQuery);
+
+// Portfolio isotope and filter
+var portfolioIsotope = $('.portfolio-container').isotope({
+	itemSelector: '.portfolio-item',
+	layoutMode: 'fitRows'
+});
+
+$('#portfolio-flters li').on('click', function () {
+	$("#portfolio-flters li").removeClass('filter-active');
+	$(this).addClass('filter-active');
+
+	portfolioIsotope.isotope({filter: $(this).data('filter')});
+});
+
 })(jQuery);
