8c87d822fb674e1a5f25ecaa1eb5b0e703058647
christian89-alt <c.duboc43@gmail.com>
2024-03-17 11:56:16 +0100

    update

--- service.php
+++ service.php
@@ -3,189 +3,155 @@ include 'partial/header.php';
 ?>
 
 
-  <!-- service section -->
-
-  <section class="service_section layout_padding">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-          Our Services
-        </h2>
-      </div>
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="box b1">
-            <div class="img-box">
-              <img src="assets/img/s1.jpg" alt="">
+<!-- Portfolio Start -->
+<div class="portfolio">
+                <div class="container">
+                    <div class="section-header">
+                        <h2>Our Portfolio</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pretium ornare velit non</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <ul id="portfolio-flters">
+                                <li data-filter="*" class="filter-active">All</li>
+                                <li data-filter=".web-des">Design</li>
+                                <li data-filter=".web-dev">Development</li>
+                                <li data-filter=".dig-mar">Digital Marketing</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row portfolio-container">
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-des">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-1.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-1.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-dev">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-2.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-2.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item dig-mar">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-3.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-3.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-des">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-4.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-4.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-des">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-1.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-1.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-dev">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-2.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-2.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item dig-mar">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-3.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-3.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-des">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-4.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-4.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-des">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-1.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-1.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-dev">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-2.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-2.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item dig-mar">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-3.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-3.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 portfolio-item web-des">
+                            <div class="portfolio-wrap">
+                                <figure>
+                                    <img src="img/portfolio-4.jpg" alt="Portfolio Image">
+                                    <a href="img/portfolio-4.jpg" class="link-preview" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                    <a href="#" class="link-details"><i class="fa fa-link"></i></a>
+                                    <a class="portfolio-title" href="#">Project Name Here</a>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 load-more">
+                            <a class="btn" href="#">Load More</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="detail-box">
-              <h5>
-                Preconstruction
-              </h5>
-              <p>
-                Veritatis tempore labore, fugit quis nihil temporibus minima. Tenetur suscipit quibusdam ab necessitatibus aut libero hic eaque dolor a.
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="box b2">
-            <div class="img-box">
-              <img src="assets/img/s2.jpg" alt="">
-            </div>
-            <div class="detail-box">
-              <h5>
-                Construction
-              </h5>
-              <p>
-                Veritatis tempore labore, fugit quis nihil temporibus minima. Tenetur suscipit quibusdam ab necessitatibus aut libero hic eaque dolor a.
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="box b3">
-            <div class="img-box">
-              <img src="assets/img/s3.jpg" alt="">
-            </div>
-            <div class="detail-box">
-              <h5>
-                Building Maintenance
-              </h5>
-              <p>
-                Veritatis tempore labore, fugit quis nihil temporibus minima. Tenetur suscipit quibusdam ab necessitatibus aut libero hic eaque dolor a.
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end service section -->
-
-  
-
-  <!-- info section -->
-  <section class="info_section ">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="info_logo">
-            <a class="navbar-brand" href="index.html">
-              <span>
-                Brincht
-              </span>
-            </a>
-            <p>
-              Libero minus ab reprehenderit atque cum praesentium placeat quos est enim facere cumque. Numquam pariatur alias
-            </p>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_links">
-            <h5>
-              Useful Link
-            </h5>
-            <ul>
-              <li>
-                <a href="">
-                  Dolor sit amet, consectetur
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  Magna aliqua. Ut enim ad
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  Minim veniam placeat
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  Quisdotempor incididunt
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_info">
-            <h5>
-              Contact Us
-            </h5>
-          </div>
-          <div class="info_contact">
-            <a href="" class="">
-              <i class="fa fa-map-marker" aria-hidden="true"></i>
-              <span>
-                Lorem ipsum dolor sit amet,
-              </span>
-            </a>
-            <a href="" class="">
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              <span>
-                Call : +01 1234567890
-              </span>
-            </a>
-            <a href="" class="">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
-              <span>
-                demo@gmail.com
-              </span>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_form ">
-            <h5>
-              Newsletter
-            </h5>
-            <form action="#">
-              <input type="email" placeholder="Enter your email">
-              <button>
-                Subscribe
-              </button>
-            </form>
-            <div class="social_box">
-              <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-youtube" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end info_section -->
+            <!-- Portfolio Start -->
 
 
-
-
-  <!-- footer section -->
-
-  <?php
+<?php
 include 'partial/footer.php';
 ?>
-  
