4c6478b8f52ffa3c95f549bd1fcd51f3c6a36b5a
christian89-alt <c.duboc43@gmail.com>
2024-03-20 13:05:44 +0100

    update

--- contact-us.php
+++ contact-us.php
@@ -44,113 +44,11 @@ include 'partial/header.php';
   <!-- end about section -->
 
   <!-- info section -->
-  <section class="info_section ">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="info_logo">
-            <a class="navbar-brand" href="index.html">
-              <span>
-                Brincht
-              </span>
-            </a>
-            <p>
-              Libero minus ab reprehenderit atque cum praesentium placeat quos est enim facere cumque. Numquam pariatur alias
-            </p>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_links">
-            <h5>
-              Useful Link
-            </h5>
-            <ul>
-              <li>
-                <a href="">
-                  Dolor sit amet, consectetur
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  Magna aliqua. Ut enim ad
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  Minim veniam placeat
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  Quisdotempor incididunt
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_info">
-            <h5>
-              Contact Us
-            </h5>
-          </div>
-          <div class="info_contact">
-            <a href="" class="">
-              <i class="fa fa-map-marker" aria-hidden="true"></i>
-              <span>
-                Lorem ipsum dolor sit amet,
-              </span>
-            </a>
-            <a href="" class="">
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              <span>
-                Call : +01 1234567890
-              </span>
-            </a>
-            <a href="" class="">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
-              <span>
-                demo@gmail.com
-              </span>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_form ">
-            <h5>
-              Newsletter
-            </h5>
-            <form action="#">
-              <input type="email" placeholder="Enter your email">
-              <button>
-                Subscribe
-              </button>
-            </form>
-            <div class="social_box">
-              <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-youtube" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end info_section -->
+  
 
 <?php
 include 'partial/footer.php';
 ?>
-</body>
+
 
 </html>
--- partial/footer.php
+++ partial/footer.php
@@ -2,7 +2,107 @@
 ?>
 
 
-
+<section class="info_section ">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="info_logo">
+            <a class="navbar-brand" href="index.html">
+              <span>
+                Offshore Service 
+              </span>
+            </a>
+            <p>
+              Développeur web Fullstack je vous accompagne dans la création de vos applications web et mobiles
+            </p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info_links">
+            <h5>
+              Liens utiles
+            </h5>
+            <ul>
+              <li>
+                <a href="https://www.francenum.gouv.fr/">
+                  France num pour vous accompagner dans la transition numérique
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Magna aliqua. Ut enim ad
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Minim veniam placeat
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Quisdotempor incididunt
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info_info">
+            <h5>
+              Contact Us
+            </h5>
+          </div>
+          <div class="info_contact">
+            <a href="" class="">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>
+                Lorem ipsum dolor sit amet,
+              </span>
+            </a>
+            <a href="" class="">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                Call : +01 1234567890
+              </span>
+            </a>
+            <a href="" class="">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span>
+                demo@gmail.com
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info_form ">
+            <h5>
+              Newsletter
+            </h5>
+            <form action="#">
+              <input type="email" placeholder="Enter your email">
+              <button>
+                Subscribe
+              </button>
+            </form>
+            <div class="social_box">
+              <a href="">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-youtube" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   <!-- footer section -->
   <footer class="container-fluid footer_section">
     <p>
