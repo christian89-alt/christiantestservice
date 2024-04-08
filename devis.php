cd96e547a108ea7fa399ddc2427260b0dd19d651
christian89-alt <c.duboc43@gmail.com>
2024-04-08 11:30:27 +0200

    update

--- devis.php
+++ devis.php
@@ -1,8 +1,34 @@
 <?php
 include 'partial/header.php';
 
+require_once(__DIR__ . '/variables.php');
+require_once(__DIR__ . '/functions.php');
 
-?>
+
+<div class= "d-flex flex-column min-vh-100">
+<div class="container">
+ 
+
+    <h1>Obtenez un devis pour vos besoins</h1>
+
+
+    <?php require_once(__DIR__ . '/login.php'); ?>
+
+    <?php if (isset($loggedUser)) : ?>
+        <?php foreach (getRecipes($recipes) as $recipe) : ?>
+            <article>
+                <h3><?php echo $recipe['title']; ?></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+            </article>
+        <?php endforeach ?>
+    <?php endif; ?>
+</div>
+</div>
+
+
+
+
 
 
 
